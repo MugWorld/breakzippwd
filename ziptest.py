cf97e5aa21be22a3e927fe28c55215a735c9f347
Mug World <18267735558@163.com>
2023-05-05 20:38:25 +0800

    Update ziptest.py

--- ziptest.py
+++ ziptest.py
@@ -1,4 +1,6 @@
 import zipfile
+#import rarfile
+from unrar import rarfile
 import multiprocessing
 
 
@@ -11,6 +13,14 @@ def extract_zip(pwd, zip_file):
     except Exception as e:
         print("密码破解失败", pwd)
 
+def extract_zip1(pwd, rar_file):
+    print("extract_zip", pwd)
+    try:
+        rar_file.extractall(pwd=str(pwd))
+        print("密码破解", pwd)
+        return pwd
+    except Exception as e:
+        print("密码破解失败", pwd)
 
 def bruteforce(zip_file):
     # pool = multiprocessing.Pool()
@@ -27,6 +37,22 @@ def bruteforce(zip_file):
     # pool.close()
     # pool.join()
 
+def bruteforce1(rar_file):
+    pool = multiprocessing.Pool()
+    for pwd in range(12278,999999999999999):
+        # print("密码破解", pwd)
+        # pool.apply_async(extract_zip1, args=(pwd, rar_file))
+        try:
+            rar_file.extractall(pwd=str(pwd))
+            print("密码破解", pwd)
+            return
+        except Exception as e:
+            #print("An exception occurred:", e)
+            print("密码破解失败", pwd)
+        # pool.apply_async(extract_zip, args=(pwd, zip_file))
+    # pool.close()
+    # pool.join()
+
 
 def pwdGen(pwdListPath):
     with open(pwdListPath, 'r', encoding="latin-1") as f:
@@ -36,12 +62,15 @@ def pwdGen(pwdListPath):
 
 def main():
     myzip = "./test1.zip"
+    myrar = "./test1.rar"
     pwd_list_path = "./0-9.8位纯数密码.txt"
     try:
-        zip_file = zipfile.ZipFile(myzip)
+        #zip_file = zipfile.ZipFile(myzip)
+        rar_file = rarfile.RarFile(myrar)
         try:
             # pwd_gen = pwdGen(pwd_list_path)
-            bruteforce(zip_file)
+            #bruteforce(zip_file)
+            bruteforce1(rar_file)
         finally:
             zip_file.close()
     except FileExistsError:
